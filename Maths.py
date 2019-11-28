14118425dca38fb6f299e900436e180cd688eced
AdrianGjonca <55891807+AdrianGjonca@users.noreply.github.com>
2019-11-28 19:46:14 +0000

    Managed to do backprop!!! :)

--- Maths.py
+++ Maths.py
@@ -1,8 +1,20 @@
 import math
 
+#Math
 def sigmoid(value):
     return 1/(1+(math.e**(-value)))
+def costOnNode(nodeValue, shouldBe):
+    return (nodeValue - shouldBe)**2
 
+def cost(outputNodes, shouldBe):
+    totalCost = 0
+    onNode = 0
+    for currentNode in outputNodes:
+        totalCost += costOnNode(currentNode.value, shouldBe[onNode])
+        onNode += 1
+    return totalCost
+
+#Classes
 class node:
     value = 0
     weights = []
@@ -30,17 +42,42 @@ class network:
                 currentLayer.compute(self.layers[onLayer-1])
             onLayer += 1
 
-def costOnNode(nodeValue, shouldBe):
-    return (nodeValue - shouldBe)**2
-
-def cost(outputNodes, shouldBe):
-    totalCost = 0
-    onNode = 0
-    for currentNode in outputNodes:
-        totalCost += costOnNode(currentNode.value, shouldBe[onNode])
-        onNode += 1
-    return totalCost
-
+#Backpropogation
+def backProp(network, correctOutput, learningRateW, learningRateB):
+    print("#Instance of Backprop#")
+    network.compute()
+    finalLayerNo = len(network.layers)-1
+    print("#Total Cost [" +str(cost(network.layers[finalLayerNo].nodes, correctOutput))+ "]#")
+    allNodesInNetwork = []
+    for currentLayer in network.layers:
+        for currentNode in currentLayer.nodes:
+            allNodesInNetwork.append(currentNode)
+    for currentNode in allNodesInNetwork:
+        counter = 0
+        #weights
+        for weight in currentNode.weights:
+            costBefore = cost(network.layers[finalLayerNo].nodes, correctOutput)
+            currentNode.weights[counter] += learningRateW
+            network.compute()
+            costAfter = cost(network.layers[finalLayerNo].nodes, correctOutput)
+            currentNode.weights[counter] -= learningRateW
+            if costAfter<costBefore:
+                currentNode.weights[counter] += learningRateW#*((costAfter-costBefore)**2)
+            if costAfter>costBefore:
+                currentNode.weights[counter] -= learningRateW#*((costAfter-costBefore)**2)
+            counter += 1
+        #bias
+        costBefore = cost(network.layers[finalLayerNo].nodes, correctOutput)
+        currentNode.bias += learningRateB
+        network.compute()
+        costAfter = cost(network.layers[finalLayerNo].nodes, correctOutput)
+        currentNode.bias -= learningRateB
+        if costAfter<costBefore:
+            currentNode.bias += learningRateB#*((costAfter-costBefore)**2)
+        if costAfter>costBefore:
+            currentNode.bias -= learningRateB#*((costAfter-costBefore)**2)
+        
+#Tests
 def test2():
     myNetwork = network()
     myNetwork.layers = [layer(),layer()]
@@ -61,7 +98,12 @@ def test2():
         print(str(i.value) + " - " + str(costOnNode(i.value, 1)))
     print()
     print(cost(myNetwork.layers[1].nodes,[1,1,1]))
-    
+    while(cost(myNetwork.layers[1].nodes,[1,1,1])>0.00001):
+        backProp(myNetwork, [1,1,1] ,0.1 , 0.01)
+        for i in myNetwork.layers[1].nodes:
+            print(str(i.value) + " - " + str(costOnNode(i.value, 1)))
+        print()
+        print(cost(myNetwork.layers[1].nodes,[1,1,1]))
         
 def test():
     layer1 = layer()
